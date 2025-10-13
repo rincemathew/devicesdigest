192273783c76cab4cbaec07c38686f3048d0ffbd
Rince Mathew <rincemathewmanattu@gmail.com>
2025-10-13 20:54:18 +0530

    started again

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="h-12">
+      <div>sign in</div>
+      <div>gadget india</div>
+      <div>search box</div>
+    </div>
+  );
 }
