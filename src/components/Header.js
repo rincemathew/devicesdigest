58c91d9433036f75c2dc76449b2b4cad946ebfd9
Rince Mathew <rincemathewmanattu@gmail.com>
2025-10-14 19:17:32 +0530

    mobile desktop setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
   return (
-    <div className="h-12">
-      <div>sign in</div>
-      <div>gadget india</div>
-      <div>search box</div>
+    <div className="flex justify-center items-center bg-(--color-main)">
+      <div className="w-full md:w-3/4 bg-(--color-secondary)">
+        <div className="hidden md:block">
+          this is desktop header
+          <div>Register or sign in</div>
+          <div>devices digest, search box, heart symbol</div>
+          <div>Mobiles and Tablets, gadgets, Articles etc</div>
+        </div>
+        <div className="md:hidden">
+          this is mobile header
+          <div>= devices digest</div>
+          <div>search box</div>
+          <div>Mobiles and Tablets, gadgets, Articles etc</div>
+        </div>
+      </div>
     </div>
   );
 }
