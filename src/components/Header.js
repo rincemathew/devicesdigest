940a6eef67c8324d231828256413e5e1240f9724
Rince Mathew <rincemathewmanattu@gmail.com>
2025-10-16 19:18:22 +0530

    desktop top register

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,32 @@
 import React from "react";
+import { LuUserRound } from "react-icons/lu";
+
 
 export default function Header() {
   return (
-    <div className="flex justify-center items-center bg-(--color-main)">
+    <header className="flex flex-col justify-center items-center bg-(--color-main)">
+      {/* Desktop Header */}
       <div className="w-full md:w-3/4 bg-(--color-secondary)">
-        <div className="hidden md:block">
-          this is desktop header
-          <div>Register or sign in</div>
+        <div className="hidden md:block pt-1">
+          <div>
+            <div className="flex justify-end text-xs">
+              <LuUserRound className="mx-1"></LuUserRound>
+              <span className="hover:underline cursor-pointer">Register</span><span className="mx-1">or</span> <span className="hover:underline cursor-pointer">Sign in</span>
+            </div>
+          </div>
+          {/* Logo and Search Box */}
           <div>devices digest, search box, heart symbol</div>
-          <div>Mobiles and Tablets, gadgets, Articles etc</div>
-        </div>
-        <div className="md:hidden">
-          this is mobile header
-          <div>= devices digest</div>
-          <div>search box</div>
-          <div>Mobiles and Tablets, gadgets, Articles etc</div>
         </div>
       </div>
-    </div>
+      {/* Navigation Bar */}
+      <nav>Mobiles and Tablets, gadgets, Articles etc</nav>
+      {/* Mobile Header */}
+      <div className="md:hidden">
+        this is mobile header
+        <div>= devices digest</div>
+        <div>search box</div>
+        <nav>Mobiles and Tablets, gadgets, Articles etc</nav>
+      </div>
+    </header>
   );
 }
